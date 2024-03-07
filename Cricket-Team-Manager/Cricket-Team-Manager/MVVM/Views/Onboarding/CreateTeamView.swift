241964defea455e62f4df9d5013f07c19c77f199
Hammad Tahir <hammadtahir203@gmail.com>
2024-03-07 14:36:51 +0500

    working on create team view

--- Cricket-Team-Manager/Cricket-Team-Manager/MVVM/Views/Onboarding/CreateTeamView.swift
+++ Cricket-Team-Manager/Cricket-Team-Manager/MVVM/Views/Onboarding/CreateTeamView.swift
@@ -8,8 +8,83 @@
 import SwiftUI
 
 struct CreateTeamView: View {
+    
+    @State var teamName : String = ""
+    @State var selectedImage: UIImage? = nil
+    @State var domesticteamName: String = ""
+    @State var email = ""
+    @State var password = ""
+    
+    
+    let imageSize: CGFloat = .width() / 3
+    
+    @State var isPresentImagePicker = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            List {
+                Section("Team Details") {
+                    
+                    VStack {
+                        Button(action: {
+                            self.isPresentImagePicker.toggle()
+                        }, label: {
+                            if let selectedImage = selectedImage {
+                                Image(uiImage: selectedImage)
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: imageSize, height: imageSize)
+                            }
+                            else {
+                                Image(systemName: "person.circle.fill")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: imageSize, height: imageSize)
+                            }
+                        })
+                        .clipShape(Circle())
+                        .foregroundStyle(.accent)
+                        .frame(maxWidth: .infinity)
+                        
+                        if selectedImage == nil {
+                            Text("Select Team Photo")
+                            
+                        }
+                    }
+                    
+                    
+                    HeaderTextField(header: "Team Name",
+                                    placeHolder: "Enter Team Name",
+                                    text: $teamName)
+                    
+                    HeaderTextField(header: "Domestic Team Name",
+                                    placeHolder: "Enter Domestic Team Name",
+                                    text: $email)
+                    
+                    HeaderTextField(header: "Email",
+                                    placeHolder: "Enter Email",
+                                    text: $email)
+                    
+                    
+                    HeaderTextField(header: "Password",
+                                    placeHolder: "Enter Password",
+                                    isPasswordField: true,
+                                    text: $password)
+                    
+                    Button {
+                        
+                    } label: {
+                        AppButton(title: "Create Team", textColor: .white)
+                    }
+                    .padding(.vertical)
+                }
+            }
+            
+        }
+        .sheet(isPresented: $isPresentImagePicker) {
+            ImagePicker(image: $selectedImage)
+        }
+        
     }
 }
 
