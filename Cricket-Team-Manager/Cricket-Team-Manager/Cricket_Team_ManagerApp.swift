d231c881d1266f6cb0fa041a0abe74c94273e499
Hamza Hashmi <68464273+hamzahashmi556@users.noreply.github.com>
2024-03-07 11:13:50 +0500

    Add files via upload
    Project Added

---
+++ Cricket-Team-Manager/Cricket-Team-Manager/Cricket_Team_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cricket_Team_ManagerApp.swift
+//  Cricket-Team-Manager
+//
+//  Created by Hamza Hashmi on 07/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Cricket_Team_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
